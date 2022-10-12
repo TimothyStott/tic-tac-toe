d7776cda4a048d29ddce48684775079482f957f3
Tim <timothy.b.stott@gmail.com>
2022-10-12 16:59:26 -0400

    Add logic for game / Add Modal
    The modal for character creation exists with no logic.
    The logic for a functioning game works, but there is no
    player logic so you can't play against an ai.

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-const winningSetArray = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[6, 4, 2]];
-const gameSquares =document.querySelectorAll(".game-square-content");
-let gameboardArray = ["0","1","X","3","X","5","X","7","8"];
-let playerArray = ["P1","P2"];
+let playerArray = ["p1", "p2"];
+const winningSetArray = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
+const gameSquares = document.querySelectorAll(".game-square-content");
+const gameSquareText = document.querySelectorAll(".game-square-text");
+const resetButton = document.getElementById("reset");
+let gameboardArray = ["", "", "", "", "", "", "", "", ""];
 let winner = undefined;
 let turn = 0;
 
 
 
-/*Event Listeners*/
-gameSquares.forEach(square => square.addEventListener('click',playerSelectionWrite))
 
+/*Event Listeners*/
+gameSquares.forEach(square => square.addEventListener('click', playerSelectionTurnKeeper))
+resetButton.addEventListener('click',resetGame);
 
 
 const Player = (name) => {
@@ -18,41 +21,95 @@ const Player = (name) => {
     const win = () => wincount++
     const resetWinCount = () => wincount = 0;
     const logWinCount = () => console.log(wincount);
-    return {_name,_wincount,win,resetWinCount,logWinCount}
+    return { _name, _wincount, win, resetWinCount, logWinCount }
 }
 
-function checkForWin(){
+function checkForWin() {
     let winnerFound = false;
-    for(let [a,b,c] of winningSetArray){
-        if(gameboardArray[a]==gameboardArray[b]&&gameboardArray[a]==gameboardArray[c]){
+    for (let [a, b, c] of winningSetArray) {
+        if (gameboardArray[a] == gameboardArray[b] && gameboardArray[a] == gameboardArray[c]) {
             winnerFound = true;
-            switch(gameboardArray[a]){
+            switch (gameboardArray[a]) {
                 case "X":
                     winner = playerArray[0];
                     console.log(winner);
                     return winnerFound;
                 case "O":
                     winner = playerArray[1];
+                    console.log(winner);
                     return winnerFound;
             }
         }
     }
 }
 
-function playerSelectionWrite(e){
-    switch(turn){
-        case 0: 
-            console.log(e);
+function checkForTie(){
+    let tieFound = true;
+    for(let i = 0; i<gameboardArray.length; i++)
+    {
+        if(gameboardArray[i] == ""){
+             tieFound = false;
+        }
+    }
+    return tieFound;
+}
+
+function playerSelectionTurnKeeper(e) {
+    checkForWin();
+    if(winner==undefined && !checkForTie()){
+    checkForValidMove(e);
+    switch (turn) {
+        case 0:
             turn++;
             break;
         case 1:
-            e.innerText = "O"
             turn--;
             break;
+        }
+    }
+    checkForWin();
+    if(winner!=undefined){
+        console.log(winner._name);
     }
 }
 
+function fillSquares() {
+    for (let i = 0; i < gameSquareText.length; i++) {
+        gameSquareText[i].innerText = gameboardArray[i];
+    };
+}
 
-function playGame(){
+function checkForValidMove(e){
+    let selected = parseInt(e.originalTarget.id);
+    if(gameboardArray[selected]==""){
+        switch(turn){
+            case 0:
+                gameboardArray[selected] = "X";
+                break;
+            case 1:
+                gameboardArray[selected] = "O"
+                break;
+        }
+    }
+    else{
+        alert("Invalid Move");
+    }
+    fillSquares();
+}
 
+function resetGame (){
+    for(let i = 0; i < gameboardArray.length; i++){
+        gameboardArray[i] = "";
+    }
+    winner = undefined;
+
+    fillSquares();
+    
+}
+
+function createPlayers(){
+    let p1 = Player("");
+    let p2 = Player("");
+    playerArray[0] = p1;
+    playerArray[1] = p2;
 }
